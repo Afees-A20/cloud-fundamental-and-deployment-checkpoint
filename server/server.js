9ab7da52135b79465dbcc88b17a172ee81a998e7
Afees Ademola <ademolaafeesy@email.com>
2026-05-10 05:34:01 +0100

    simplify azure deployment option

--- server/server.js
+++ server/server.js
@@ -1,37 +1,27 @@
 require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 
 const app = express();
-
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API running" });
+});
 
 async function startServer() {
   try {
-    // MongoDB connection
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-
-    const distPath = path.join(__dirname, '../client/dist');
-
-    // Serve frontend
-    app.use(express.static(distPath));
-
-    // ROUTE
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(distPath, 'index.html'));
-    });
+    console.log("MongoDB connected");
 
     const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, '0.0.0.0', () => {
-      console.log(`Server running on port ${PORT}`);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log("Server running on port", PORT);
     });
 
   } catch (err) {
-    console.error('Server failed:', err);
+    console.error(err);
   }
 }
 
