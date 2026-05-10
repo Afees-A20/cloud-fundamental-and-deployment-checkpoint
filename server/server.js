201d0012aa556dc5dd2c029a9ee74472a9e219c9
Afees Ademola <ademolaafeesy@email.com>
2026-05-10 05:43:38 +0100

    revert back to mono repo deployment

--- server/server.js
+++ server/server.js
@@ -1,27 +1,37 @@
 require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "API running" });
-});
+app.use(express.json());
 
 async function startServer() {
   try {
+    // MongoDB connection
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    console.log('MongoDB connected');
+
+    const distPath = path.join(__dirname, '../client/dist');
+
+    // Serve frontend
+    app.use(express.static(distPath));
+
+    // ROUTE
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(distPath, 'index.html'));
+    });
 
     const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log("Server running on port", PORT);
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server running on port ${PORT}`);
     });
 
   } catch (err) {
-    console.error(err);
+    console.error('Server failed:', err);
   }
 }
 
