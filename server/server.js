e446b5f911fc49a1f856cc05ddd8c81eb0aadf8d
Afees Ademola <ademolaafeesy@email.com>
2026-05-10 03:49:15 +0100

    server file error fixed

--- server/server.js
+++ server/server.js
@@ -6,36 +6,32 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+
 async function startServer() {
-
   try {
-
     // MongoDB connection
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log('MongoDB connected');
 
-    // Vite build folder
     const distPath = path.join(__dirname, '../client/dist');
 
-    // Serve frontend files
+    // Serve frontend
     app.use(express.static(distPath));
 
-    // React routes
-    app.get('/{*any}', (req, res) => {
+    // ROUTE
+    app.get('*', (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
     });
 
     const PORT = process.env.PORT || 5000;
 
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
 
   } catch (err) {
-
-    console.error(err);
-
+    console.error('Server failed:', err);
   }
 }
 
